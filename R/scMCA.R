d1e140328a7586284192b79fc3372600e16cdceb
Yincong Zhou <yczhou@zju.edu.cn>
2018-01-14 03:31:38 +0800

    fix some bugs

--- R/scMCA.R
+++ R/scMCA.R
@@ -1,15 +1,15 @@
 ##------------------------------------------------------------------------------------------------------
-##cytosee
 
 #' Mouse Cell Atlas mapping
-#' description hello world
+#' description
 #' @param scdata data.frame or matrix, col correspond to cells and rows correspond to genes
 #' @param plot_heat default TRUE, boolean, this will return the result alongwith a heatmap, else will return in a list
 #' @param interactive_plot defalut is TRUE, boolean, IF TRUE, a plotly package will be used for interactive plotting
 #' @param numbers_plot default is 3, number, it will return top "numbers_plot" records in the plot
 #' @importFrom reshape2 melt
-#' @importFrom ggplot2 ggplot
+#' @import ggplot2
 #' @importFrom plotly ggplotly
+#' @import pheatmap
 #' @export
 
 scMCA <- function(scdata,plot_heat=TRUE,interactive_plot=TRUE,numbers_plot=3){
@@ -22,20 +22,28 @@ scMCA <- function(scdata,plot_heat=TRUE,interactive_plot=TRUE,numbers_plot=3){
   tst.expr[is.na(tst.expr)]<-0
   cors <- cor(ref.expr,tst.expr)
 
-  cors_index <- apply(cors,2,gettissue)
+  cors_index <- apply(cors,2,gettissue,numbers_plot)
   cors_index <- sort(unique(as.integer(cors_index)))
   scblast.result <- apply(cors,2,function(x) rownames(cors)[which.max(x)])
   cors_in = cors[cors_index,]
   colnames(cors_in)<-colnames(scdata)
   result<-list()
   if(isTRUE(plot_heat)){
-    p=plotMCA(cors_in)
+    p<-pheatmap(
+      cors_in,
+      clustering_distance_rows = "euclidean",
+      clustering_distance_cols = "euclidean",
+      clustering_method = "ward.D",
+      fontsize_col = 0.001,
+      fontsize_row = 8,
+      color = colorRampPalette(c("grey", "white", "red"))(100))
     if(isTRUE(interactive_plot)){
-      return(ggplotly(p))
+      map_interact<-plotMCA(cors_in[p$tree_row$order,p$tree_col$order])
+      return(ggplotly(map_interact))
     }
-    else{
-    }
-    return(p)
+    result[['scMCA']]<-scblast.result
+    result[['scMCA_probility']]<-cors_in
+    return(result)
 
   }
   else{
@@ -55,6 +63,8 @@ gettissue <- function(x,Num=3){
   return(top_markers)
 }
 
+#' @importFrom reshape2 melt
+#' @import ggplot2
 #' @export
 plotMCA <- function(data){
   df_heatmap1 <- melt(t(data))
