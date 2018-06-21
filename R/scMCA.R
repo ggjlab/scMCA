9500c01133c5ab38d8ea76ae1cb1ca230131c0e6
Yincong Zhou <yczhou@zju.edu.cn>
2018-06-21 21:15:51 +0800

    update scMCA function

--- R/scMCA.R
+++ R/scMCA.R
@@ -7,12 +7,9 @@
 #' @param interactive_plot defalut is TRUE, boolean, IF TRUE, a plotly package will be used for interactive plotting
 #' @param numbers_plot default is 3, number, it will return top "numbers_plot" records in the plot
 #' @importFrom reshape2 melt
-#' @import ggplot2
-#' @importFrom plotly ggplotly
-#' @import pheatmap
 #' @export
 
-scMCA <- function(scdata,plot_heat=TRUE,interactive_plot=TRUE,numbers_plot=3){
+scMCA <- function(scdata,numbers_plot=3, shiny_ui = T){
   tst.expr <- data.frame(matrix(nrow = dim(ref.expr)[1],ncol=dim(scdata)[2]))
   rownames(tst.expr)<-rownames(ref.expr)
   colnames(tst.expr)<-colnames(scdata)
@@ -21,41 +18,24 @@ scMCA <- function(scdata,plot_heat=TRUE,interactive_plot=TRUE,numbers_plot=3){
   }
   tst.expr[is.na(tst.expr)]<-0
   tst.expr<-log(tst.expr+1)
-  cors <- cor(ref.expr,tst.expr)
+  cors <- cor(log(ref.expr+1),tst.expr)
 
   cors_index <- apply(cors,2,gettissue,numbers_plot)
   cors_index <- sort(unique(as.integer(cors_index)))
   scblast.result <- apply(cors,2,function(x) rownames(cors)[which.max(x)])
   cors_in = cors[cors_index,]
   colnames(cors_in)<-colnames(scdata)
-  result<-list()
-  if(isTRUE(plot_heat)){
-    p<-pheatmap(
-      cors_in,
-      clustering_distance_rows = "euclidean",
-      clustering_distance_cols = "euclidean",
-      clustering_method = "ward.D",
-      fontsize_col = 0.001,
-      fontsize_row = 8,
-      color = colorRampPalette(c("grey", "white", "red"))(100))
-    if(isTRUE(interactive_plot)){
-      map_interact<-plotMCA(cors_in[p$tree_row$order,p$tree_col$order])
-      return(ggplotly(map_interact))
-    }
-    result[['scMCA']]<-scblast.result
-    result[['scMCA_probility']]<-cors_in
-    return(result)
-
-  }
-  else{
-    result[['scMCA']]<-scblast.result
-    result[['scMCA_probility']]<-cors_in
-    return(result)
-  }
+  cors_out = reshape2::melt(cors_in)[c(2,1,3)]
+  colnames(cors_out)<- c("Cell","Cell type","Score")
+  cors_out <- as.data.frame(cors_out %>% group_by(Cell) %>% top_n(n=numbers_plot,wt=Score))
 
 
-
-
+  result <- list()
+  result[["cors_matrix"]] <- cors
+  result[['top_cors']]<-numbers_plot
+  result[['scMCA']]<-scblast.result
+  result[['scMCA_probility']]<-cors_out
+  return(result)
 }
 
 #' @export
@@ -64,23 +44,47 @@ gettissue <- function(x,Num=3){
   return(top_markers)
 }
 
+#' @import dplyr
 #' @importFrom reshape2 melt
-#' @import ggplot2
+#' @importFrom  plotly plot_ly
+#' @importFrom pheatmap pheatmap
 #' @export
-plotMCA <- function(data){
+plotMCA <- function(mca_result,interactive_plot=F, numbers_plot=3, col_font_size = 1, row_font_size=8, show_col=T,show_bar=T, show_tree = T){
   data(ref.expr)
-  df_heatmap1 <- melt(t(data))
-  colnames(df_heatmap1)<-c('cell','type','value')
-  p<-ggplot(df_heatmap1, aes(cell,type )) +
-    geom_tile(aes(fill = value), color = "white") +
-    scale_fill_gradientn(colours = c('grey','white','red')) +
-    ylab("Types") +
-    xlab("Cells") +
-    theme(legend.title = element_text(size = 10),
-          legend.text = element_text(size = 12),
-          plot.title = element_text(size=16),
-          axis.title=element_text(size=14,face="bold"),
-          axis.text.x = element_text(angle = 90, hjust = 1)) +
-    labs(fill = "level")
-    return(p)
+  cors <- mca_result$cors_matrix
+  cors_index <- apply(cors,2,gettissue,numbers_plot)
+  cors_index <- sort(unique(as.integer(cors_index)))
+  data = cors[cors_index,]
+  cors_out = reshape2::melt(data)[c(2,1,3)]
+  colnames(cors_out)<- c("Cell","Cell type","Score")
+  cors_out <- as.data.frame(cors_out %>% group_by(Cell) %>% top_n(n=numbers_plot,wt=Score))
+  mca_result$scMCA_probility <- cors_out
+  mca_result$top_cors <- numbers_plot
+  height=dim(data)[1]*10+230
+  tree_height = 0
+  if(isTRUE(show_tree)){tree_height=50}
+
+    p<-pheatmap(
+      data,
+      clustering_distance_rows = "euclidean",
+      clustering_distance_cols = "euclidean",
+      clustering_method = "ward.D",
+      fontsize_col = col_font_size,
+      fontsize_row = row_font_size,
+      color = colorRampPalette(c("grey", "white", "red"))(100),
+      cellheight = 10,
+      show_colnames = show_col,
+      border_color = NA,
+      height = height,
+      legend = show_bar,
+      treeheight_col = tree_height,
+      treeheight_row = tree_height
+      )
+    if(isTRUE(interactive_plot)){
+
+      inter_data<-data[rev(p$tree_row$order),][,p$tree_col$order]
+      height= length(p$tree_row$order)*10+230
+      plot_ly(x=colnames(inter_data),y=rownames(inter_data),z = inter_data, colors = colorRamp(c("grey", "white","red")),height=height, type = "heatmap", showscale=show_bar) %>% layout(autosize=T,  margin=list(l=0,r=230,b=180,t=20,pad=4),font=list(size=row_font_size),xaxis=list(showticklabels=show_col),yaxis=list(side="right"))
+    }
+
 }
